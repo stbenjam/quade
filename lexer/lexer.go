748aebc8adcb38febd27d517dbbc4db09ea0598f
Stephen Benjamin <stephen@redhat.com>
2018-06-13 20:11:17 -0400

    More tokens for lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -13,30 +13,70 @@ type TokenType int
 
 const (
 	Error TokenType = iota
-	StartQuad
-	CloseQuad
-
+	OpenParen
+	CloseParen
+	Comma
 	Temporary
 	Identifier
-
-	// Operations
-	OpAddSigned
+	Number
+	OpAddressOf
+	OpLoadWord
+	OpLoadHalfWord
+	OpLoadSignedHalfWord
+	OpLoadByte
+	OpLoadSignedByte
+	OpStoreWord
+	OpStoreHalfWord
+	OpStoreByte
+	OpMultSignedWord
+	OpMultUnsignedWord
+	OpDivSignedWord
+	OpDivUnsignedWord
+	OpRemSignedWord
+	OpRemUnsignedWord
+	OpAddSignedWord
+	OpAddUnsignedWord
+	OpSubSignedWord
+	OpSubUnsignedWord
+	OpLeftShiftWord
+	OpRightShiftSignedWord
+	OpRightShiftUnsignedWord
+	OpLtSignedWord
+	OpLtUnsignedWord
+	OpLeSignedWord
+	OpLeUnsignedWord
+	OpGeSignedWord
+	OpGeUnsignedWord
+	OpGtSignedWord
+	OpGtUnsignedWord
+	OpEqWord
+	OpNeWord
+	OpBitwiseAndWord
+	OpBitwiseXorWord
+	OpBitwiseOrWord
+	OpUnaryMinus
+	OpUnaryLogicalNegation
+	OpUnaryBitwiseNegation
+	OpConstInt
+	OpCastWordToHalfWord
+	OpCastWordToByte
+	OpCastHalfWordToUnsignedWord
+	OpCastHalfWordToSignedWord
+	OpCastByteToUnsignedWord
+	OpCastByteToSignedWord
+	OpLabel
+	OpGoto
+	OpGotoIfFalse
+	OpGotoIfTrue
+	OpPhi
 )
-
-var TokenString = map[TokenType]string{
-	Error:     `Error`,
-	StartQuad: `StartQuad`,
-	CloseQuad: `CloseQuad`,
-
-	Temporary:   `Temporary`,
-	Identifier:  `Identifier`,
-	OpAddSigned: `OpAddSigned`,
-}
 
 type Token struct {
 	Type  TokenType
 	Value string
 }
+
+type yySymType Token
 
 type lexState func(*lexer) lexState
 
@@ -46,6 +86,68 @@ type lexer struct {
 	position int
 	tokens   chan Token
 }
+
+var TokenString = map[TokenType]string{
+	Error:                        `Error`,
+	OpenParen:                    `OpenParen`,
+	CloseParen:                   `CloseParen`,
+	Comma:                        `Comma`,
+	Temporary:                    `Temporary`,
+	Identifier:                   `Identifier`,
+	Number:                       `Number`,
+	OpAddressOf:                  `OpAddressOf`,
+	OpLoadWord:                   `OpLoadWord`,
+	OpLoadHalfWord:               `OpLoadHalfWord`,
+	OpLoadSignedHalfWord:         `OpLoadSignedHalfWord`,
+	OpLoadByte:                   `OpLoadByte`,
+	OpLoadSignedByte:             `OpLoadSignedByte`,
+	OpStoreWord:                  `OpStoreWord`,
+	OpStoreHalfWord:              `OpStoreHalfWord`,
+	OpStoreByte:                  `OpStoreByte`,
+	OpMultSignedWord:             `OpMultSignedWord`,
+	OpMultUnsignedWord:           `OpMultUnsignedWord`,
+	OpDivSignedWord:              `OpDivSignedWord`,
+	OpDivUnsignedWord:            `OpDivUnsignedWord`,
+	OpRemSignedWord:              `OpRemSignedWord`,
+	OpRemUnsignedWord:            `OpRemUnsignedWord`,
+	OpAddSignedWord:              `OpAddSignedWord`,
+	OpAddUnsignedWord:            `OpAddUnsignedWord`,
+	OpSubSignedWord:              `OpSubSignedWord`,
+	OpSubUnsignedWord:            `OpSubUnsignedWord`,
+	OpLeftShiftWord:              `OpLeftShiftWord`,
+	OpRightShiftSignedWord:       `OpRightShiftSignedWord`,
+	OpRightShiftUnsignedWord:     `OpRightShiftUnsignedWord`,
+	OpLtSignedWord:               `OpLtSignedWord`,
+	OpLtUnsignedWord:             `OpLtUnsignedWord`,
+	OpLeSignedWord:               `OpLeSignedWord`,
+	OpLeUnsignedWord:             `OpLeUnsignedWord`,
+	OpGeSignedWord:               `OpGeSignedWord`,
+	OpGeUnsignedWord:             `OpGeUnsignedWord`,
+	OpGtSignedWord:               `OpGtSignedWord`,
+	OpGtUnsignedWord:             `OpGtUnsignedWord`,
+	OpEqWord:                     `OpEqWord`,
+	OpNeWord:                     `OpNeWord`,
+	OpBitwiseAndWord:             `OpBitwiseAndWord`,
+	OpBitwiseXorWord:             `OpBitwiseXorWord`,
+	OpBitwiseOrWord:              `OpBitwiseOrWord`,
+	OpUnaryMinus:                 `OpUnaryMinus`,
+	OpUnaryLogicalNegation:       `OpUnaryLogicalNegation`,
+	OpUnaryBitwiseNegation:       `OpUnaryBitwiseNegation`,
+	OpConstInt:                   `OpConstInt`,
+	OpCastWordToHalfWord:         `OpCastWordToHalfWord`,
+	OpCastWordToByte:             `OpCastWordToByte`,
+	OpCastHalfWordToUnsignedWord: `OpCastHalfWordToUnsignedWord`,
+	OpCastHalfWordToSignedWord:   `OpCastHalfWordToSignedWord`,
+	OpCastByteToUnsignedWord:     `OpCastByteToUnsignedWord`,
+	OpCastByteToSignedWord:       `OpCastByteToSignedWord`,
+	OpLabel:                      `OpLabel`,
+	OpGoto:                       `OpGoto`,
+	OpGotoIfFalse:                `OpGotoIfFalse`,
+	OpGotoIfTrue:                 `OpGotoIfTrue`,
+	OpPhi:                        `OpPhi`,
+}
+
+
 
 const Debug = 1
 const eof = -1
@@ -74,16 +176,31 @@ func startState(l *lexer) lexState {
 
 	for {
 		r := l.next()
-		switch r {
-		case '(':
-			l.emit(StartQuad)
-			return insideQuadIR
-		case '#', '`':
+		switch {
+		case r == '(':
+			l.emit(OpenParen)
+			return startState
+		case r == ')':
+			l.emit(CloseParen)
+			return startState
+		case r == ',':
+			l.emit(Comma)
+			return startState
+		case r == '#':
 			return insideComment
-		case ' ', '\t', '\n':
+		case r == 't':
+			if n := l.peek(); unicode.IsDigit(n) {
+				return temporary
+			}
+			return identifierOrOperation
+		case r == ' ' || r == '\t' || r == '\n':
 			continue
-		case eof:
+		case unicode.IsLetter(r) || r == '_':
+			return identifierOrOperation
+		case r == eof:
 			return nil
+		case unicode.IsDigit(r):
+			return number
 		}
 	}
 
@@ -108,32 +225,169 @@ func insideComment(l *lexer) lexState {
 func temporary(l *lexer) lexState {
 	debug("STATE: Temporary")
 
-	l.emit(Temporary)
-	return nil
-}
-
-func insideQuadIR(l *lexer) lexState {
-	debug("STATE: Inside Quad IR")
-
+FOR:
 	for {
-		switch r := l.next(); r {
-		case eof:
-			return l.err("unexpected eof")
-		case ')':
-			l.emit(CloseQuad)
-			return startState
-		case 't':
-			if n := l.peek(); unicode.IsDigit(n) {
-				return temporary
-			}
+		r := l.next()
 
-			return nil
-			// case digit
-			//      return number
-			// case default
-			//      return identifierOrOperation
+		switch {
+		case r == eof:
+			break FOR
+		case unicode.IsLetter(r) || r == '_':
+			// Oops this is an identifierOrOperation
+			return identifierOrOperation
+		case !unicode.IsDigit(r):
+			l.unput(r)
+			break FOR
 		}
 	}
+
+	l.emit(Temporary)
+	return startState
+}
+
+func identifierOrOperation(l *lexer) lexState {
+	debug("STATE: Identifier or Operation")
+
+	for {
+		r := l.next()
+
+		if r == eof {
+			break
+		}
+
+		if !(unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_') {
+			l.unput(r)
+			break
+		}
+	}
+
+	switch l.value() {
+	case "addressOf":
+		l.emit(OpAddressOf)
+	case "loadWord":
+		l.emit(OpLoadWord)
+	case "loadHalfWord":
+		l.emit(OpLoadHalfWord)
+	case "loadSignedHalfWord":
+		l.emit(OpLoadSignedHalfWord)
+	case "loadByte":
+		l.emit(OpLoadByte)
+	case "loadSignedByte":
+		l.emit(OpLoadSignedByte)
+	case "storeWord":
+		l.emit(OpStoreWord)
+	case "storeHalfWord":
+		l.emit(OpStoreHalfWord)
+	case "storeByte":
+		l.emit(OpStoreByte)
+	case "multSignedWord":
+		l.emit(OpMultSignedWord)
+	case "multUnsignedWord":
+		l.emit(OpMultUnsignedWord)
+	case "divSignedWord":
+		l.emit(OpDivSignedWord)
+	case "divUnsignedWord":
+		l.emit(OpDivUnsignedWord)
+	case "remSignedWord":
+		l.emit(OpRemSignedWord)
+	case "remUnsignedWord":
+		l.emit(OpRemUnsignedWord)
+	case "addSignedWord":
+		l.emit(OpAddSignedWord)
+	case "addUnsignedWord":
+		l.emit(OpAddUnsignedWord)
+	case "subSignedWord":
+		l.emit(OpSubSignedWord)
+	case "subUnsignedWord":
+		l.emit(OpSubUnsignedWord)
+	case "leftShiftWord":
+		l.emit(OpLeftShiftWord)
+	case "rightShiftSignedWord":
+		l.emit(OpRightShiftSignedWord)
+	case "rightShiftUnsignedWord":
+		l.emit(OpRightShiftUnsignedWord)
+	case "ltSignedWord":
+		l.emit(OpLtSignedWord)
+	case "ltUnsignedWord":
+		l.emit(OpLtUnsignedWord)
+	case "leSignedWord":
+		l.emit(OpLeSignedWord)
+	case "leUnsignedWord":
+		l.emit(OpLeUnsignedWord)
+	case "geSignedWord":
+		l.emit(OpGeSignedWord)
+	case "geUnsignedWord":
+		l.emit(OpGeUnsignedWord)
+	case "gtSignedWord":
+		l.emit(OpGtSignedWord)
+	case "gtUnsignedWord":
+		l.emit(OpGtUnsignedWord)
+	case "eqWord":
+		l.emit(OpEqWord)
+	case "neWord":
+		l.emit(OpNeWord)
+	case "bitwiseAndWord":
+		l.emit(OpBitwiseAndWord)
+	case "bitwiseXorWord":
+		l.emit(OpBitwiseXorWord)
+	case "bitwiseOrWord":
+		l.emit(OpBitwiseOrWord)
+	case "unaryMinus":
+		l.emit(OpUnaryMinus)
+	case "unaryLogicalNegation":
+		l.emit(OpUnaryLogicalNegation)
+	case "unaryBitwiseNegation":
+		l.emit(OpUnaryBitwiseNegation)
+	case "constInt":
+		l.emit(OpConstInt)
+	case "castWordToHalfWord":
+		l.emit(OpCastWordToHalfWord)
+	case "castWordToByte":
+		l.emit(OpCastWordToByte)
+	case "castHalfWordToUnsignedWord":
+		l.emit(OpCastHalfWordToUnsignedWord)
+	case "castHalfWordToSignedWord":
+		l.emit(OpCastHalfWordToSignedWord)
+	case "castByteToUnsignedWord":
+		l.emit(OpCastByteToUnsignedWord)
+	case "castByteToSignedWord":
+		l.emit(OpCastByteToSignedWord)
+	case "label":
+		l.emit(OpLabel)
+	case "goto":
+		l.emit(OpGoto)
+	case "gotoIfFalse":
+		l.emit(OpGotoIfFalse)
+	case "gotoIfTrue":
+		l.emit(OpGotoIfTrue)
+	case "phi":
+		l.emit(OpPhi)
+
+	default:
+		l.emit(Identifier)
+	}
+
+	return startState
+}
+
+func number(l *lexer) lexState {
+	debug("STATE: Number")
+
+	for {
+		r := l.next()
+
+		if r == eof {
+			break
+		}
+
+		if !(unicode.IsDigit(r) || r == 'x') {
+			l.unput(r)
+			break
+		}
+	}
+
+	l.emit(Number)
+	return startState
 }
 
 func (l *lexer) run() {
@@ -147,16 +401,23 @@ func (l *lexer) run() {
 	debug("Done")
 }
 
+func (l *lexer) value() string {
+	return l.input[l.start:l.position]
+}
+
 func (l *lexer) emit(token TokenType) {
-	value := l.input[l.start:l.position]
+	debug(fmt.Sprintf("Token: %s, Value: %s", TokenString[token], l.value()))
 
-	debug(fmt.Sprintf("Token: %s, Value: %s", TokenString[token], value))
-
-	l.tokens <- Token{token, value}
+	l.tokens <- Token{token, l.value()}
 	l.start = l.position
 }
 
 func (l *lexer) next() rune {
+	if l.position > len(l.input) {
+		l.position -= 1
+		return eof
+	}
+
 	r, width := utf8.DecodeRuneInString(l.input[l.position:])
 
 	if r == utf8.RuneError {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,25 +1,46 @@
 package lexer_test
 
 import (
-    "fmt"
 	"github.com/stbenjam/quade/lexer"
 	"testing"
 )
 
-func TestBadInput(t *testing.T) {
-    validateLexerReturnsToken(t, "bad input", lexer.Error)
-}
-
 func TestTokens(t *testing.T) {
-    validateLexerReturnsToken(t, "(", lexer.StartQuad)
+	expectTokens(t, "(", lexer.OpenParen)
+	expectTokens(t, ")", lexer.CloseParen)
+	expectTokens(t, ",", lexer.Comma)
+
+	expectTokens(t, "_identifier", lexer.Identifier)
+	expectTokens(t, "9number", lexer.Number, lexer.Identifier)
+	expectTokens(t, "number9", lexer.Identifier)
+
+	expectTokens(t, "t0", lexer.Temporary)
+	expectTokens(t, "t01892", lexer.Temporary)
+	expectTokens(t, "t99", lexer.Temporary)
+	expectTokens(t, "t9a", lexer.Identifier)
+	expectTokens(t, "t9,a", lexer.Temporary, lexer.Comma, lexer.Identifier)
+	expectTokens(t, "t", lexer.Identifier)
+
+	expectTokens(t, "(addSignedWord, t0, t1, t2)", lexer.OpenParen, lexer.OpAddSignedWord, lexer.Comma, lexer.Temporary, lexer.Comma, lexer.Temporary, lexer.Comma, lexer.Temporary, lexer.CloseParen)
 }
 
+func TestComments(t *testing.T) {
+    expectTokens(t, "(  # (ignored, t0, t1, t2)", lexer.OpenParen)
+}
 
-func validateLexerReturnsToken(t *testing.T, input string, expectedToken lexer.TokenType) {
+func expectTokens(t *testing.T, input string, expectedTokens ...lexer.TokenType) {
 	tokenStream := lexer.Lex(input)
-	token := <-tokenStream
 
-	if token.Type != expectedToken {
-        t.Errorf(fmt.Sprintf("Did not receive expected token. Expected %s, got %s.  :-(", lexer.TokenString[expectedToken], lexer.TokenString[token.Type]))
+	for _, expectedToken := range expectedTokens {
+		token := <-tokenStream
+		if token.Type != expectedToken {
+			t.Errorf("Did not receive expected token. Expected %s, got %s.  :-(", lexer.TokenString[expectedToken], lexer.TokenString[token.Type])
+		}
+	}
+
+	received, ok := <-tokenStream
+
+	if ok {
+		t.Errorf("There are more tokens in the channel then expected.  Next token: %s", lexer.TokenString[received.Type])
 	}
 }
