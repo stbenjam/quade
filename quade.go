2d30ea3a81e3cc7f297f0a050813e23cb012babe
Stephen Benjamin <stephen@redhat.com>
2018-08-18 20:42:35 -0400

    Feed file to lexer

--- quade.go
+++ quade.go
@@ -3,10 +3,31 @@
 package main
 
 import (
-//"fmt"
-//"github.com/stbenjam/quade/quade"
+    "github.com/stbenjam/quade/lexer"
+    "github.com/stbenjam/quade/parser"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
 )
 
 func main() {
+    //output := flag.String("output", "STDOUT", "Write output to a file")
+    flag.Parse()
 
+    if(len(flag.Args()) == 0) {
+        flag.Usage()
+        os.Exit(1)
+    }
+
+    buf, err := ioutil.ReadFile(flag.Args()[0])
+    if(err != nil) {
+        log.Fatal(err)
+    }
+
+    tokenStream := lexer.Lex(string(buf))
+    for token := range tokenStream {
+        log.Print(fmt.Sprintf("RECEIVED TOKEN: %s\n", parser.TokenName(token.Type)))
+    }
 }
